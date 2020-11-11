74104fdf681a234ede0a6e5c7d617082a02397d3
Hairo Garcia <69333686+xpeedy@users.noreply.github.com>
2020-11-11 15:42:56 -0500

    form-structure

---
+++ form-app/src/Components/Form.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+
+export default function Form(){
+
+    return(
+        <form>
+            <label>Name
+                <input />
+            </label>
+            <br/>
+            <label>Email
+                <input />
+            </label>
+            <br/>
+            <label>Password
+                <input />
+            </label>
+            <br/>
+            <label>Terms of service
+                <input />
+            </label>
+            <br/>
+            <button>Submit</button>
+        </form>
+
+    )
+}
